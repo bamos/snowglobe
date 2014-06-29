85c5545f459517db7accd10fd09af2e831be1130
Brandon Amos <bamos@cmu.edu>
2014-06-28 17:00:44 -0700

    Add environment variables to env.sh.

---
+++ env.sh
@@ -0,0 +1,10 @@
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_05.jdk/Contents/Home
+
+CF=http://d2io1hx8u877l0.cloudfront.net
+COL_DIR=2-collectors/scala-stream-collector
+COL=snowplow-stream-collector-0.1.0
+ENR_DIR=3-enrich/scala-kinesis-enrich
+ENR=snowplow-kinesis-enrich-0.1.0
+GL=http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
+CF_2=http://d1fc8wv8zag5ca.cloudfront.net/1
+SP=sp.js
