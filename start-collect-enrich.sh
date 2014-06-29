0c223af3fdfd49d8b80153b62d70d8b7123a551e
Brandon Amos <bamos@cmu.edu>
2014-06-28 17:01:19 -0700

    Initial commit of the start script to pipe output to events.tsv.

---
+++ start-collect-enrich.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+function die() { echo $*; exit -1; }
+
+cd $(dirname $0)
+source env.sh
+JAVA_VER=$(java -version 2>&1 | \
+  sed 's/java version "\(.*\)\.\(.*\)\..*"/\1\2/; 1q')
+[ "$JAVA_VER" -gt 16 ] || die "Java version must be greater than 1.6."
+
+./$COL --config collector.conf | ./$ENR --config enrich.conf 1>> events.tsv
