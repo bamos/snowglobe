63592070595da6b3941e544531ef92eeef35f286
Brandon Amos <bdamos@vt.edu>
2015-05-02 12:16:03 -0400

    Minor formatting and refactoring.

--- analysis/Main.hs
+++ analysis/Main.hs
@@ -39,23 +39,20 @@ getTodaysEvents:: TimeZone -> LocalTime -> V.Vector EnrichedEvent
         -> V.Vector EnrichedEvent
 getTodaysEvents tz now = V.filter (isToday tz now)
 
-dailyEvents:: TimeZone -> LocalTime -> V.Vector EnrichedEvent -> IO()
-dailyEvents tz now events = print $ V.length $ getTodaysEvents tz now events
+numDailyEvents:: TimeZone -> LocalTime -> V.Vector EnrichedEvent -> Int
+numDailyEvents tz now events = V.length $ getTodaysEvents tz now events
 
 analytics:: Args -> IO()
 analytics args = do
   csvData <- BL.readFile $ events args
   tz <- getCurrentTimeZone
   now <- utcToLocalTime tz <$> getCurrentTime
-  case decodeCsv csvData of
-    Left err -> putStrLn err
-    Right events ->
-        case mode args of
-          "NumDailyEvents" -> dailyEvents tz now events
-          m -> putStrLn $ "Error: Unexpected mode: " ++ m
+  case (mode args, decodeCsv csvData) of
+    (_, Left err) -> putStrLn err
+    ("NumDailyEvents", Right events) -> print $ numDailyEvents tz now events
+    (m, _) -> putStrLn $ "Error: Unexpected mode: " ++ m
 
 main :: IO ()
 main = execParser opts >>= analytics
-    where
-      opts = info (helper <*> args)
-             ( fullDesc <> header "SnowGlobe Analytics" )
+    where opts = info (helper <*> args)
+                 (fullDesc <> header "SnowGlobe Analytics")
