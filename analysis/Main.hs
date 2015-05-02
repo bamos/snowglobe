19eab7ae1b946bfa8e9d210acc7427213d2c7a51
Brandon Amos <bdamos@vt.edu>
2015-05-02 11:06:33 -0400

    Add command line argument parsing.

--- analysis/Main.hs
+++ analysis/Main.hs
@@ -7,11 +7,25 @@ import Data.Time(Day, LocalTime(..), TimeZone,
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Vector as V
 
+import Options.Applicative
+
 import SnowGlobe.EnrichedEvent
 import SnowGlobe.Time(parse)
 
+data Args = Args
+    { events :: String
+    , mode :: String
+    } deriving (Show)
+
+args :: Parser Args
+args = Args <$> strOption (long "events" <> metavar "FILE" <>
+                                help "Location of events.tsv" )
+       <*> strOption (long "mode" <> metavar "MODE" <>
+                           help "One of: NumDailyEvents" )
+
+
 decodeCsv:: BL.ByteString -> Either String (V.Vector EnrichedEvent)
-decodeCsv dat = decodeWith opts NoHeader dat
+decodeCsv = decodeWith opts NoHeader
     where opts = defaultDecodeOptions {decDelimiter = fromIntegral $ ord '\t'}
 
 isToday:: TimeZone -> LocalTime -> EnrichedEvent -> Bool
@@ -19,16 +33,29 @@ isToday tz now e =
     case eTimeM of
       Nothing -> False
       Just eTime -> localDay now == eTime
-    where eTimeM = fmap (localDay) $ parse tz $ collectorTstamp e :: Maybe Day
+    where eTimeM = fmap localDay $ parse tz $ collectorTstamp e :: Maybe Day
 
 getTodaysEvents:: TimeZone -> LocalTime -> V.Vector EnrichedEvent
         -> V.Vector EnrichedEvent
-getTodaysEvents tz now events = V.filter (isToday tz now) events
+getTodaysEvents tz now = V.filter (isToday tz now)
 
-main = do
-  csvData <- BL.readFile "../data/events.tsv"
+dailyEvents:: TimeZone -> LocalTime -> V.Vector EnrichedEvent -> IO()
+dailyEvents tz now events = print $ V.length $ getTodaysEvents tz now events
+
+analytics:: Args -> IO()
+analytics args = do
+  csvData <- BL.readFile $ events args
   tz <- getCurrentTimeZone
   now <- utcToLocalTime tz <$> getCurrentTime
   case decodeCsv csvData of
     Left err -> putStrLn err
-    Right events -> print $ V.length $ getTodaysEvents tz now events
+    Right events ->
+        case mode args of
+          "NumDailyEvents" -> dailyEvents tz now events
+          m -> putStrLn $ "Error: Unexpected mode: " ++ m
+
+main :: IO ()
+main = execParser opts >>= analytics
+    where
+      opts = info (helper <*> args)
+             ( fullDesc <> header "SnowGlobe Analytics" )
