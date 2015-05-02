a1ad1e4421bebb6eff1668227d2f22038ebd906a
Brandon Amos <bdamos@vt.edu>
2015-05-02 10:29:00 -0400

    Filter events for today.

--- analysis/Main.hs
+++ analysis/Main.hs
@@ -1,7 +1,8 @@
 import Data.Char(ord)
 import Data.Csv (HasHeader(NoHeader), decodeWith, decDelimiter,
                  defaultDecodeOptions)
-import Data.Time(getCurrentTimeZone)
+import Data.Time(Day, LocalTime(..), TimeZone,
+                    getCurrentTime, getCurrentTimeZone, utcToLocalTime)
 
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Vector as V
@@ -13,9 +14,21 @@ decodeCsv:: BL.ByteString -> Either String (V.Vector EnrichedEvent)
 decodeCsv dat = decodeWith opts NoHeader dat
     where opts = defaultDecodeOptions {decDelimiter = fromIntegral $ ord '\t'}
 
+isToday:: TimeZone -> LocalTime -> EnrichedEvent -> Bool
+isToday tz now e =
+    case eTimeM of
+      Nothing -> False
+      Just eTime -> localDay now == eTime
+    where eTimeM = fmap (localDay) $ parse tz $ collectorTstamp e :: Maybe Day
+
+getTodaysEvents:: TimeZone -> LocalTime -> V.Vector EnrichedEvent
+        -> V.Vector EnrichedEvent
+getTodaysEvents tz now events = V.filter (isToday tz now) events
+
 main = do
   csvData <- BL.readFile "../data/events.tsv"
   tz <- getCurrentTimeZone
+  now <- utcToLocalTime tz <$> getCurrentTime
   case decodeCsv csvData of
     Left err -> putStrLn err
-    Right v -> V.forM_ v $ \p -> print $ parse tz $ collectorTstamp p
+    Right events -> print $ V.length $ getTodaysEvents tz now events
