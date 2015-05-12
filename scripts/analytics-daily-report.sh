90c296884dbecb93376b5e825cb022951d8d3eda
Brandon Amos <bdamos@vt.edu>
2015-05-12 14:04:57 -0400

    Update SnowGlobe arguments in cron scripts.

--- scripts/analytics-daily-report.sh
+++ scripts/analytics-daily-report.sh
@@ -5,5 +5,5 @@ SNOWGLOBE=$HOME/repos/snowglobe
 $SNOWGLOBE/dist/build/snowglobe-analysis/snowglobe-analysis \
   --geoDB $SNOWGLOBE/vendor/GeoLiteCity.dat \
   --events $SNOWGLOBE/data/events.tsv \
-  --mode DayReport \
+  DayReport \
   | mutt bdamos@vt.edu -s "Analytics Report for $(date +%Y-%m-%d)"
--- scripts/analytics-weekly-report.sh
+++ scripts/analytics-weekly-report.sh
@@ -5,5 +5,5 @@ SNOWGLOBE=$HOME/repos/snowglobe
 $SNOWGLOBE/dist/build/snowglobe-analysis/snowglobe-analysis \
   --geoDB $SNOWGLOBE/vendor/GeoLiteCity.dat \
   --events $SNOWGLOBE/data/events.tsv \
-  --mode WeekReport \
+  WeekReport \
   | mutt bdamos@vt.edu -s "Analytics Weekly Report for $(date +%Y-%m-%d)"
