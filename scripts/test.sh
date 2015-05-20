d59ea021428ba7f5c1604b0d977d8096b1227d5c
Brandon Amos <bamos@cs.cmu.edu>
2015-05-20 10:57:58 -0400

    Initial commit of testing complete pipeline in travis.

---
+++ scripts/test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+set -x -e # Output commands being executed and propogate errors.
+cd $(dirname $0)/.. # cd into the snowglobe
+
+./scripts/bootstrap.sh
+./scripts/start-collect-enrich.sh &
+./dist/build/snowglobe-analysis/snowglobe-analysis \
+  --events data/events.tsv \
+  --mode DayReport
+
+# TODO: Kill collecter and enricher subprocess
