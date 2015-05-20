728545d4a9686bf72e5201eccc7bf0cdbb2633bb
Brandon Amos <bamos@cs.cmu.edu>
2015-05-20 16:18:57 -0400

    travis: (For now) sleep for a second after launching collector background process

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,6 +5,7 @@ cd $(dirname $0)/.. # cd into the snowglobe
 
 ./scripts/bootstrap.sh
 ./scripts/start-collect-enrich.sh &
+sleep 1
 ./dist/build/snowglobe-analysis/snowglobe-analysis \
   --events data/events.tsv \
   DayReport
