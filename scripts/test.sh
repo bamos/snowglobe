915863c767e9d0e19cf4c319e33b2af3957c8215
Brandon Amos <bdamos@vt.edu>
2015-05-21 16:03:52 -0400

    Travis: Sleep for a minute.

--- scripts/test.sh
+++ scripts/test.sh
@@ -11,15 +11,16 @@ sed "s|home/bamos/repos/snowglobe|$PWD|g" \
 mv conf/enrichments/ip_lookups{.new,}.json
 
 #./scripts/start-collect-enrich.sh &
+mkdir -p data
 stdbuf -i0 -o0 -e0 \
   ./vendor/snowplow-stream-collector-0.4.0 --config ./conf/collector.conf \
   | ./vendor/snowplow-kinesis-enrich-0.5.0 \
     --config ./conf/enrich.conf \
     --resolver file:./conf/resolver.json \
-    --enrichments file:./conf/enrichments &
+    --enrichments file:./conf/enrichments > data/events.tsv &
 CE_PID=$!
 
-sleep 10 # Be sure the collector and enricher are initialized.
+sleep 60 # Be sure the collector and enricher are initialized.
 ./sample/send-view.rb # Send a single page view.
 
 SG=./dist/build/snowglobe-analysis/snowglobe-analysis
