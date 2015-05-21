aefc17337dd924055fdbc81877c4a2772fd3c3f4
Brandon Amos <bamos@cs.cmu.edu>
2015-05-21 11:52:17 -0400

    Add send-view ruby script

---
+++ sample/send-view.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+
+require 'snowplow-tracker'
+
+e = SnowplowTracker::Emitter.new('localhost:8080')
+t = SnowplowTracker::Tracker.new(e)
+t.track_page_view('http://test.com', "Test page title")
