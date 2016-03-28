65a3ee2d1cec8c36699f02365ac94296322e6521
Brandon Amos <bamos@cs.cmu.edu>
2016-03-27 22:13:33 -0400

    start-collect-enrich: Fix JAVA_VER regex for openjdk

--- scripts/start-collect-enrich.sh
+++ scripts/start-collect-enrich.sh
@@ -5,7 +5,7 @@ function die() { echo $*; exit -1; }
 cd $(dirname $0)
 source env.sh
 JAVA_VER=$(java -version 2>&1 | \
-  sed 's/java version "\(.*\)\.\(.*\)\..*"/\1\2/; 1q')
+  sed 's/.* version "\(.*\)\.\(.*\)\..*"/\1\2/; 1q')
 [ "$JAVA_VER" -gt 16 ] || die "Java version must be greater than 1.6."
 
 mkdir -p ../data
